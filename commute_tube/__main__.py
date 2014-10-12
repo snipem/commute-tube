0883f848bc9628240761325c5e15d7a1f073f8de
snipem <mail@matthias-kuech.de>
2014-10-12 12:36:53 +0200

    Modularization and option of checking for USB pen added

--- commute_tube/__main__.py
+++ commute_tube/__main__.py
@@ -1,81 +1,16 @@
-#!/usr/bin/env python
+from commute_tube import CommuteTube
+import argparse
 
-from youtube_dl import YoutubeDL
-import youtube_dl
-import os
-import sys
-import logging
-import json
-from pprint import pprint
-import subprocess
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
 
+	ct = CommuteTube()
 
-debug = False
+	parser.add_argument('-c', action='store_true', help='Check if USB pen is present')
+	args = parser.parse_args()
 
-log = logging.getLogger()
-ydlLog = logging.getLogger("youtube_dl")
-
-if debug == True:
-	log.setLevel("DEBUG")
-
-def mountUSB(path):
-	subprocess.check_call(["mount", path])
-	return True
-
-def unmountUSB(path):
-	subprocess.check_call(["umount", path])
-	return True
-
-def getConfig():
-	json_data=open('config.json')
-	return json.load(json_data)
-
-config = getConfig()
-
-penPath = config['pen']['penPath']
-downloadFolder = config['pen']['downloadFolder']
-pathToDownloadFolder = penPath+"/"+downloadFolder
-
-if os.path.ismount(penPath) == False:
-	print ("There is no USB pen mounted under " + penPath + ". Trying to mount it.")
-	outcomeMount = mountUSB(penPath)
-	
-	if outcomeMount == False:
-		print ("Could not mount USB pen under " + penPath + ". Exiting")
-		sys.exit(-1)
+	if (args.c == True):
+		ct.checkForPen()
 	else:
-		print ("Successfully mounted USB pen under " + penPath)
-else:
-	print ("There is a USB pen already mounted under " + penPath + ". Processing further.")
-
-if os.path.exists(pathToDownloadFolder) == False:
-	print ("Creating folder "+pathToDownloadFolder)
-	os.mkdir(pathToDownloadFolder)
-
-ydl = YoutubeDL()
-
-ydl.add_default_info_extractors()
-
-for source in config['source']:
-	print source
-	
-	ydl.params = source
-	ydl.params['nooverwrites'] = True
-	ydl.params['download_archive'] = "already_downloaded.txt"
-	#ydl.params['logger'] = ydlLog
-	outtmpl = pathToDownloadFolder + u'/%(uploader)s-%(title)s-%(id)s.%(ext)s'
-	ydl.params['outtmpl'] = outtmpl
-
-	if debug == True:
-		log.debug("All downloads will be simulated since this is debug mode")
-		ydl.params['simulate'] = True
-
-	ydl.download([source['url']])
-
-if unmountUSB(penPath) == True:
-	print ("USB Pen under "+penPath+ " has been unmounted")
-	sys.exit(0)
-else:
-	print ("USB Pen under "+penPath+ " has not been Successfully unmounted")
-	sys.exit(1)
+		ct.main()
 
---
+++ commute_tube/commute_tube.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+from youtube_dl import YoutubeDL
+import youtube_dl
+import os
+import sys
+import logging
+import json
+from pprint import pprint
+import subprocess
+
+class CommuteTube():
+
+	debug = None
+	log = None
+	ydlLog = None
+	config = []
+	penPath = ""
+	downloadFolder = ""
+	pathToDownloadFolder = ""
+
+
+	def getConfig(self):
+		json_data=open('config.json')
+		return json.load(json_data)
+
+	def __init__(self):
+
+		self.debug = True
+		self.log = logging.getLogger()
+		self.ydlLog = logging.getLogger("youtube_dl")
+		self.config = self.getConfig()
+
+		self.penPath = self.config['pen']['penPath']
+		self.downloadFolder = self.config['pen']['downloadFolder']
+		self.pathToDownloadFolder = self.penPath+"/"+self.downloadFolder
+
+		if self.debug == True:
+			self.log.setLevel("DEBUG")
+
+	def mountUSB(self,path):
+		subprocess.check_call(["mount", path])
+		return True
+
+	def unmountUSB(self,path):
+		subprocess.check_call(["umount", path])
+		return True
+
+	def run(self):
+
+		if os.path.ismount(self.penPath) == False:
+			print ("There is no USB pen mounted under " + self.penPath + ". Trying to mount it.")
+			outcomeMount = self.mountUSB(self.penPath)
+			
+			if outcomeMount == False:
+				print ("Could not mount USB pen under " + self.penPath + ". Exiting")
+				sys.exit(-1)
+			else:
+				print ("Successfully mounted USB pen under " + self.penPath)
+		else:
+			print ("There is a USB pen already mounted under " + self.penPath + ". Processing further.")
+
+		if os.path.exists(self.pathToDownloadFolder) == False:
+			print ("Creating folder "+self.pathToDownloadFolder)
+			os.mkdir(self.pathToDownloadFolder)
+
+		ydl = YoutubeDL()
+
+		ydl.add_default_info_extractors()
+
+		for source in self.config['source']:
+			print source
+			
+			ydl.params = source
+			ydl.params['nooverwrites'] = True
+			ydl.params['download_archive'] = "already_downloaded.txt"
+			#ydl.params['logger'] = ydlLog
+			outtmpl = self.pathToDownloadFolder + u'/%(uploader)s-%(title)s-%(id)s.%(ext)s'
+			ydl.params['outtmpl'] = outtmpl
+
+			if self.debug == True:
+				self.log.debug("All downloads will be simulated since this is debug mode")
+				ydl.params['simulate'] = True
+
+			ydl.download([source['url']])
+
+		if self.unmountUSB(self.penPath) == True:
+			print ("USB Pen under "+self.penPath+ " has been unmounted")
+			sys.exit(0)
+		else:
+			print ("USB Pen under "+self.penPath+ " has not been successfully unmounted")
+			sys.exit(1)
+
+	def checkForPen(self):
+		if os.path.ismount(self.penPath) == False:
+			print("USB Pen is not mounted under "+self.penPath)
+			if self.mountUSB(self.penPath) == True:
+				print("USB Pen has been successfully mounted")
+			else:
+				sys.exit(1)
+
+			if self.unmountUSB(self.penPath) == True:
+				print("USB Pen has been successfully unmounted")
+			else:
+				sys.exit(1)
+
+			print ("USB Pen is present and able to be mounted")
+			sys.exit(0)
+		else:
+			print("USB Pen is already mounted under "+ self.penPath)
+			sys.exit(0)
+
+	def main(self):
+		self.run()
+
+
