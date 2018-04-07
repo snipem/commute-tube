24d9c08131924091e3cbba922695cdbbe61add20
Matthias Küch <mail@matthias-kuech.de>
2018-04-07 10:25:13 +0200

    Consistent naming

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='commutetube',
+    name='commute-tube',
     version='0.1dev',
     packages=['commute_tube',],
     license='GPL 3.0',
     long_description=open('README.md').read(),
     entry_points = {
         "console_scripts": [
-            "commutetube=commute_tube.__main__:main"
+            "commute-tube=commute_tube.__main__:main"
         ]
     },
     install_requires = [
